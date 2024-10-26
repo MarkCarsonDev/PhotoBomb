d58f1ab75416d5d6478223d883dd4a73d9e0ff49
ViniciusDugue <viniciusdugue08@gmail.com>
2024-10-26 11:46:53 -0700

    Commit 1

---
+++ test_test.py
@@ -0,0 +1,10 @@
+import face_recognition
+import unittest
+import os
+import numpy as np
+from click.testing import CliRunner
+
+from face_recognition import api
+from face_recognition import face_recognition_cli
+from face_recognition import face_detection_cli
+
