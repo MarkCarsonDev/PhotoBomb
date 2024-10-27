587158db1503a0135519b9cecbe793edff1eee6f
ViniciusDugue <viniciusdugue08@gmail.com>
2024-10-26 22:03:39 -0700

    Bruh

---
+++ PhotoBombApp/app/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "expo-router";
+import { NavigationContainer } from '@react-navigation/native';
+import { Pressable, View } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Tabs, Link } from 'expo-router';
+
+
+
+
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name = "index" />
+      <Stack.Screen name = "settings" />
+      </Stack>
+  );
+}
